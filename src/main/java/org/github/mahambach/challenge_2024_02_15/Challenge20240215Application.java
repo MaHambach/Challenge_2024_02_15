606577bdb2684b58bab2b1b36f7ef2c1a4653672
mahambach <ma.hambach@gmx.net>
2024-02-15 10:47:33 +0100

    Initial commit

---
+++ src/main/java/org/github/mahambach/challenge_2024_02_15/Challenge20240215Application.java
@@ -0,0 +1,13 @@
+package org.github.mahambach.challenge_2024_02_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge20240215Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge20240215Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/github/mahambach/challenge_2024_02_15/Challenge20240215ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.mahambach.challenge_2024_02_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge20240215ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
