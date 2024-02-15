3c0579b75923e96b49ee735f9dcfac9d7dd11199
mahambach <ma.hambach@gmx.net>
2024-02-15 11:57:57 +0100

    add POST, PUT and DELETE support to "AsterixController.java"
    refactor "AsterixCharacter.java" with lombok "@With"

--- src/main/java/org/github/mahambach/challenge_2024_02_15/AsterixCharacter.java
+++ src/main/java/org/github/mahambach/challenge_2024_02_15/AsterixCharacter.java
@@ -1,6 +1,8 @@
 package org.github.mahambach.challenge_2024_02_15;
 
+import lombok.With;
 import org.springframework.data.annotation.Id;
 
+@With
 public record AsterixCharacter(@Id String id, String name, int age, String occupation) {
 }
--- src/main/java/org/github/mahambach/challenge_2024_02_15/controllers/AsterixController.java
+++ src/main/java/org/github/mahambach/challenge_2024_02_15/controllers/AsterixController.java
@@ -8,17 +8,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/asterix")
+@RequestMapping("/api/asterix/characters")
 @RequiredArgsConstructor
 public class AsterixController {
     private final AsterixRepo repo;
-    @GetMapping("/characters")
+    @GetMapping
     public List<AsterixCharacter> getAsterix() {
         return repo.findAll();
     }
 
-    @PostMapping("/add")
-    public void addAsterix(@RequestBody AsterixCharacter character) {
+    @PostMapping
+    public AsterixCharacter addAsterixCharacter(@RequestBody AsterixCharacter character) {
         repo.save(character);
+        return character;
+    }
+
+    @PutMapping("/{id}")
+    public AsterixCharacter updateAsterixCharacter(@PathVariable String id, @RequestBody AsterixCharacter character) {
+        character = character.withId(id);
+        repo.save(character);
+        return character;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteAsterixCharacter(@PathVariable String id) {
+        repo.deleteById(id);
+        return "Character with id " + id + " was removed.";
     }
 }
