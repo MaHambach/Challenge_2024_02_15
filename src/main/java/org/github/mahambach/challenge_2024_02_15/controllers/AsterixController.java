fde4bfc28f82a390c161146c49a521178a6dbfc8
mahambach <ma.hambach@gmx.net>
2024-02-15 11:24:54 +0100

    add mongodb connetion refactor "AsterixController.java"

--- src/main/java/org/github/mahambach/challenge_2024_02_15/controllers/AsterixController.java
+++ src/main/java/org/github/mahambach/challenge_2024_02_15/controllers/AsterixController.java
@@ -1,5 +1,6 @@
 package org.github.mahambach.challenge_2024_02_15.controllers;
 
+import org.github.mahambach.challenge_2024_02_15.AsterixCharacter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/asterix")
 public class AsterixController {
-    @GetMapping
-    public String getAsterix() {
+    @GetMapping("/characters")
+    public List<AsterixCharacter> getAsterix() {
         return List.of(
-                new Character("1", "Asterix", 35, "Krieger"),
-                new Character("2", "Obelix", 35, "Lieferant"),
-                new Character("3", "Miraculix", 60, "Druide"),
-                new Character("4", "Majestix", 60, "Häuptling"),
-                new Character("5", "Troubadix", 25, "Barden"),
-                new Character("6", "Gutemine", 35, "Häuptlingsfrau"),
-                new Character("7", "Idefix", 5, "Hund"),
-                new Character("8", "Geriatrix", 70, "Rentner"),
-                new Character("9", "Automatix", 35, "Schmied"),
-                new Character("10", "Grockelix", 35, "Fischer")
+                new AsterixCharacter("1", "Asterix", 35, "Krieger"),
+                new AsterixCharacter("2", "Obelix", 35, "Lieferant"),
+                new AsterixCharacter("3", "Miraculix", 60, "Druide"),
+                new AsterixCharacter("4", "Majestix", 60, "Häuptling"),
+                new AsterixCharacter("5", "Troubadix", 25, "Barden"),
+                new AsterixCharacter("6", "Gutemine", 35, "Häuptlingsfrau"),
+                new AsterixCharacter("7", "Idefix", 5, "Hund"),
+                new AsterixCharacter("8", "Geriatrix", 70, "Rentner"),
+                new AsterixCharacter("9", "Automatix", 35, "Schmied"),
+                new AsterixCharacter("10", "Grockelix", 35, "Fischer")
         );
     }
 }
