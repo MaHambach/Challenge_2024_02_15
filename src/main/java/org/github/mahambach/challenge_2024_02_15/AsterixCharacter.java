a87d333adbf0497a90ab4a26bc3f186d37778388
Ugur Kartal <kartalugur@gmail.com>
2024-02-15 11:52:07 +0100

    update AsterixCharacter

--- src/main/java/org/github/mahambach/challenge_2024_02_15/AsterixCharacter.java
+++ src/main/java/org/github/mahambach/challenge_2024_02_15/AsterixCharacter.java
@@ -1,4 +1,6 @@
 package org.github.mahambach.challenge_2024_02_15;
 
-public record AsterixCharacter(String id, String name, int age, String profession) {
+import org.springframework.data.annotation.Id;
+
+public record AsterixCharacter(@Id String id, String name, int age, String occupation) {
 }
